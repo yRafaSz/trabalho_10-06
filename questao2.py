49a17cff390f9b81b4f9eae0491b3bad5c43c261
yRafaSz <rafaelfilipe.2012@hotmail.com>
2025-05-29 10:54:46 -0300

    Add files via upload

---
+++ questao2.py
@@ -0,0 +1,7 @@
+celsius = int(input("Digite a temperatura em Celsius: "))
+Fahrenheit = celsius * 9/5 + 32
+
+print()
+print("A temperatura em Fahrenheit é:", Fahrenheit)
+print()
+input("Digite uma tecla para sair...")
